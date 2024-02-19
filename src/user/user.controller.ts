74462700488bd582a984aef8fbbb2493010c5828
Leonardo <leowitcroz@gmail.com>
2024-02-19 15:54:56 -0300

    first commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body){
+        return {
+            body
+        }
+    }
+    
+}
---
+++ src/user/user.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { UserController } from "./user.controller";
+
+@Module({
+    imports: [],
+    controllers:[UserController],
+    providers:[],
+    exports:[],
+})
+export class UserModule {
+
+
+}
