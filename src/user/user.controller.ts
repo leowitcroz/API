6707f5ae7183723d2d3af298ca9bbfbf6667f637
Leonardo <leowitcroz@gmail.com>
2024-02-20 13:27:12 -0300

    update and read

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,45 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Patch, Post, Put } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
 
     @Post()
-    async create(@Body() body){
+    async create(@Body() body) {
         return {
             body
         }
     }
-    
+
+    @Get()
+    async read() {
+        return {
+            users: []
+        }
+    }
+
+    @Get(':id')
+    async readOne(@Param() params) {
+        return {
+            user: [],
+            params
+        }
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() params) {
+        return {
+            body,
+            params
+        }
+    }
+
+    @Patch(':id')
+    async updateParcial(@Body() body, @Param() params) {
+        return {
+            body,
+            params
+        }
+    }
+
+
 }
