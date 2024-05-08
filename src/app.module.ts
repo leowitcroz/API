09069c731cf7a71bdc66f2d416619069e59eacd2
Leonardo <leowitcroz@gmail.com>
2024-05-08 15:10:10 -0300

    instalacao do mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
@@ -13,7 +15,20 @@ import { ConfigModule } from '@nestjs/config';
     ThrottlerModule.forRoot([{
     ttl: 60,
     limit: 10,
-  }]), forwardRef(() => AuthModule), forwardRef(() => UserModule)],
+  }]), forwardRef(() => AuthModule), forwardRef(() => UserModule),
+   MailerModule.forRoot({
+    transport: 'smtps://vinnie.bashirian@ethereal.email:QPeeSrYKxBxepdS52X@smtp.ethereal.email',
+    defaults: {
+      from: '"nest-modules" <modules@nestjs.com>',
+    },
+    template: {
+      dir: __dirname + '/templates',
+      adapter: new PugAdapter(),
+      options: {
+        strict: true,
+      },
+    },
+  }),],
   controllers: [AppController],
   providers: [AppService,
     {
